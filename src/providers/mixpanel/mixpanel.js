c5e46810531b1c34964192347959e7086cf41b3e
Calvin French-Owen <calvin@segment.io>
2013-02-05 19:11:55 -0800

    Updating mixpanel

--- src/providers/mixpanel/mixpanel.js
+++ src/providers/mixpanel/mixpanel.js
@@ -32,12 +32,6 @@ analytics.addProvider('Mixpanel', {
         (function (c, a) {
             window.mixpanel = a;
             var b, d, h, e;
-            b = c.createElement('script');
-            b.type = 'text/javascript';
-            b.async = true;
-            b.src = ('https:' === c.location.protocol ? 'https:' : 'http:') + '//segmentio.github.com/analytics.js/src/providers/mixpanel/mixpanel-2.2.js';
-            d = c.getElementsByTagName('script')[0];
-            d.parentNode.insertBefore(b, d);
             a._i = [];
             a.init = function (b, c, f) {
                 function d(a, b) {
@@ -50,11 +44,17 @@ analytics.addProvider('Mixpanel', {
                 var g = a;
                 'undefined' !== typeof f ? g = a[f] = [] : f = 'mixpanel';
                 g.people = g.people || [];
-                h = ['disable', 'track', 'track_pageview', 'track_links', 'track_forms', 'register', 'register_once', 'unregister', 'identify', 'alias', 'name_tag', 'set_config', 'people.set', 'people.increment'];
+                h = ['disable', 'track', 'track_pageview', 'track_links', 'track_forms', 'register', 'register_once', 'unregister', 'identify', 'alias', 'name_tag', 'set_config', 'people.set', 'people.increment', 'people.track_charge', 'people.append'];
                 for (e = 0; e < h.length; e++) d(g, h[e]);
                 a._i.push([b, c, f]);
             };
             a.__SV = 1.2;
+            b = c.createElement('script');
+            b.type = 'text/javascript';
+            b.async = true;
+            b.src = ('https:' === c.location.protocol ? 'https:' : 'http:') + '//segmentio.github.com/analytics.js/src/providers/mixpanel/mixpanel-2.2.js';
+            d = c.getElementsByTagName('script')[0];
+            d.parentNode.insertBefore(b, d);
         })(document, window.mixpanel || []);
 
         // Pass settings directly to `init` as the second argument.
